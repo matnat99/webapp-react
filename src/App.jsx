bafa5c75615ece71b5dbd82675565e1f3391111e
Mattia Natale <mattianatale07@gmail.com>
2025-02-26 16:48:44 +0100

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Layouts
+import DefaultLayout from "./layouts/DefaultLayout";
+
+// Pages
+import HomePage from "./pages/Homepage";
+import MoviePage from "./pages/MoviePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
